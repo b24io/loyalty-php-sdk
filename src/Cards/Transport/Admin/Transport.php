151f46ea1da093d54aa76b4006b9180205410d23
B24io <app@b24.io>
2018-11-30 01:05:51 +0300

    task#4526 add card add operation

--- src/Cards/Transport/Admin/Transport.php
+++ src/Cards/Transport/Admin/Transport.php
@@ -49,4 +49,42 @@ class Transport extends SDK\Transport\AbstractTransport
 
         return $cardResponse;
     }
+
+    /**
+     * @param int                      $cardNumber
+     * @param SDK\Users\DTO\UserId     $userId
+     * @param SDK\Transport\DTO\Reason $reason
+     *
+     * @return Cards\Transport\DTO\CardResponse
+     * @throws SDK\Exceptions\ApiClientException
+     * @throws SDK\Exceptions\BaseLoyaltyException
+     * @throws SDK\Exceptions\NetworkException
+     * @throws SDK\Exceptions\UnknownException
+     */
+    public function addNewCardWithCardNumber(int $cardNumber, SDK\Users\DTO\UserId $userId, SDK\Transport\DTO\Reason $reason): Cards\Transport\DTO\CardResponse
+    {
+        $this->log->debug('b24io.loyalty.sdk.cards.transport.admin.addNewCardWithCardNumber.start', [
+            'cardNumber' => $cardNumber,
+            'userId' => $userId->getId(),
+            'reason' => SDK\Transport\Formatters\Reason::toArray($reason),
+        ]);
+
+        $requestResult = $this->apiClient->executeApiRequest(
+            'admin/card/add',
+            RequestMethodInterface::METHOD_POST,
+            SDK\Cards\Formatters\AddCard::toArray(SDK\Cards\Operations\Fabric::createAddCardOperation($cardNumber, $reason, $userId))
+        );
+
+        $cardResponse = new SDK\Cards\Transport\DTO\CardResponse(
+            Cards\DTO\Fabric::initFromArray($requestResult['result']),
+            $this->initMetadata($requestResult['meta'])
+        );
+
+        $this->log->debug('b24io.loyalty.sdk.cards.transport.admin.addNewCardWithCardNumber.finish', [
+            'cardNumber' => $cardResponse->getCard()->getNumber(),
+            'metadata' => SDK\Transport\Formatters\Metadata::toArray($cardResponse->getMeta()),
+        ]);
+
+        return $cardResponse;
+    }
 }
