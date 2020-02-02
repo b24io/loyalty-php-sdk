89ea566bcaef6206eb174cfcd70186da27b10961
B24io <loyalty@b24.io>
2020-02-02 12:19:25 +0300

    issue#6 fix php 7.4 compatible errors

--- src/Users/DTO/Email.php
+++ src/Users/DTO/Email.php
@@ -27,7 +27,7 @@ final class Email
     public function __construct(string $email)
     {
         $email = strtolower($email);
-        $this->username = implode(explode('@', $email, -1), '@');
+        $this->username = implode('@', explode('@', $email, -1));
         $this->domain = str_replace($this->username . '@', '', $email);
     }
 
