bd9d9872f1f0d6e333f462ef4376fbf30c7167de
B24io <loyalty@b24.io>
2023-12-18 00:18:30 +0600

    add transactions bulk processing

--- src/Commands/Transactions/BulkTransactions.php
+++ src/Commands/Transactions/BulkTransactions.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace B24io\Loyalty\SDK\Commands\Transactions;
 
+use B24io\Loyalty\SDK\Common\Reason;
+use B24io\Loyalty\SDK\Common\TransactionType;
 use B24io\Loyalty\SDK\Services\ServiceBuilderFactory;
+use Money\Currencies\ISOCurrencies;
+use Money\Currency;
+use Money\Formatter\DecimalMoneyFormatter;
+use Money\Parser\DecimalMoneyParser;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -12,6 +18,8 @@ use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Throwable;
 
 #[AsCommand(
     name: 'transactions:bulk-transaction',
@@ -42,40 +50,115 @@ class BulkTransactions extends Command
             InputOption::VALUE_REQUIRED,
             'API admin key from application settings');
         $this->addOption(
-            'type',
+            'transaction-type',
             null,
             InputOption::VALUE_REQUIRED,
             'transaction type: accrual or payment');
+        $this->addOption(
+            'amount',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'transaction amount'
+        );
+        $this->addOption(
+            'currency',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'ISO currency code'
+        );
+        $this->addOption(
+            'reason-code',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'reason code'
+        );
+        $this->addOption(
+            'reason-comment',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'reason comment'
+        );
+        $this->addOption(
+            'dryrun',
+            null,
+            InputOption::VALUE_NONE,
+            'validate input and show information',
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln([
-            'Bulk operations with active cards',
+            'Apply transaction to all active cards',
             '============',
             '',
         ]);
+        $io = new SymfonyStyle($input, $output);
+        $decimalMoneyParser = new DecimalMoneyParser(new ISOCurrencies());
+        $decimalMoneyFormatter = new DecimalMoneyFormatter(new ISOCurrencies());
+
 
         $apiEndpointUrl = $input->getOption('api-endpoint-url');
         if ($apiEndpointUrl === null) {
-            $output->writeln('error: you must set api-endpoint-url option');
+            $io->error('you must set «api-endpoint-url» option');
 
             return Command::INVALID;
         }
 
         $apiClientId = $input->getOption('api-client-id');
         if ($apiClientId === null) {
-            $output->writeln('error: you must set api-client-id option');
+            $io->error('you must set «api-client-id» option');
 
             return Command::INVALID;
         }
 
         $apiAdminKey = $input->getOption('api-admin-key');
         if ($apiAdminKey === null) {
-            $output->writeln('error: you must set api-admin-key option');
+            $io->error('you must set «api-admin-key» option');
 
             return Command::INVALID;
         }
+
+        $transactionType = TransactionType::tryFrom((string)$input->getOption('transaction-type'));
+        if ($transactionType === null) {
+            $io->error('you must set «transaction-type» option: «accrual» or «payment»');
+
+            return Command::INVALID;
+        }
+
+        $currency = $input->getOption('currency');
+        if ($currency === null) {
+            $io->error('you must set «currency» option, use ISO-currency codes');
+
+            return Command::INVALID;
+        }
+        $currency = new Currency($currency);
+
+        $amount = $input->getOption('amount');
+        if ($amount === null) {
+            $io->error('you must set «amount» option');
+
+            return Command::INVALID;
+        }
+        $amount = $decimalMoneyParser->parse($amount, $currency);
+
+        $reasonCode = $input->getOption('reason-code');
+        if ($reasonCode === null) {
+            $io->error('you must set «reason-code» option');
+
+            return Command::INVALID;
+        }
+
+        $reasonComment = $input->getOption('reason-comment');
+        if ($reasonComment === null) {
+            $io->error('you must set «reason-comment» option');
+
+            return Command::INVALID;
+        }
+        $isDryrun = $input->getOption('dryrun');
+
+
+        // add result trx log
 
         $admSb = ServiceBuilderFactory::createAdminRoleServiceBuilder(
             $this->logger,
@@ -83,11 +166,53 @@ class BulkTransactions extends Command
             $apiClientId,
             $apiAdminKey
         );
-
         $cardsTotal = $admSb->cardsScope()->cards()->count();
-        $progressBar = new ProgressBar($output, $cardsTotal);
+        $io->info([
+            '',
+            sprintf('amount: %s', $decimalMoneyFormatter->format($amount)),
+            sprintf('cards affected: %s', $cardsTotal),
+            sprintf('reason code: %s', $reasonCode),
+            sprintf('reason comment: %s', $reasonComment),
+        ]);
 
+        $progressBar = new ProgressBar($output, $cardsTotal);
         foreach ($admSb->cardsScope()->fetcher()->list() as $card) {
+            try {
+                if ($isDryrun) {
+                    $io->note(['', 'dry run mode is active', '']);
+                    break;
+                }
+
+                $trxHistoryByCard = $admSb->transactionsScope()->transactions()->getByCardNumber($card->number);
+                $reasonCodeHistory = array_map(static function (Reason $reason) {
+                    return $reason->code;
+                }, array_column($trxHistoryByCard->getTransactions(), 'reason'));
+                if (in_array($reasonCode, $reasonCodeHistory, true)) {
+                    $this->logger->info(sprintf('transaction already processed for card %s', $card->number));
+                    continue;
+                }
+
+                $trxResult = $admSb->transactionsScope()->transactions()->processAccrualTransactionByCardNumber(
+                    $card->number,
+                    $amount,
+                    new Reason(
+                        'loyalty-php-sdk',
+                        $reasonCode,
+                        $reasonComment
+                    )
+                );
+                $this->logger->info(
+                    sprintf('transaction for card %s processed', $card->number), [
+                    'trxId' => $trxResult->getTransaction()->id->toRfc4122()
+                ]);
+            } catch (Throwable $exception) {
+                $io->error([
+                    '',
+                    $exception->getMessage(),
+                    $exception->getTraceAsString()
+                ]);
+            }
+
             $progressBar->advance();
         }
         $progressBar->finish();
--- src/Common/Result/Transactions/TransactionsResult.php
+++ src/Common/Result/Transactions/TransactionsResult.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace B24io\Loyalty\SDK\Common\Result\Transactions;
 
+use B24io\Loyalty\SDK\Core\Exceptions\BaseException;
 use B24io\Loyalty\SDK\Core\Result\AbstractResult;
 
 class TransactionsResult extends AbstractResult
 {
+    /**
+     * @return TransactionItemResult[]
+     * @throws BaseException
+     */
     public function getTransactions(): array
     {
         $res = [];
