1586844aade15583930c7a9eef39f9b7135c082a
B24io <app@b24.io>
2024-05-22 19:55:04 +0600

    Add rector configuration file
    This commit introduces a new rector.php file to manage the rector configuration. With this update, paths to the /src and /tests directories are defined, PHP sets are specified, and a rule to add void return types to methods with no return is set up.
    
    Signed-off-by: B24io <app@b24.io>

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+     ->withPhpSets(
+         false, // 8.3
+         true   // 8.2
+     )
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ]);
